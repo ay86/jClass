f9deec9f67f83c2d6738f0654f764ebc96c39c25
Angus.Young <angusyoung@qq.com>
2016-08-29 17:22:42 +0800

    v1.0.6
    * update $.ajax support type="HEAD" response

--- res/jClass-ajax.js
+++ res/jClass-ajax.js
@@ -80,29 +80,54 @@
 					jConfig['url'] += '?' + aQuery.join('&');
 				}
 				xhr.open(jConfig['type'], jConfig['url'], jConfig['async']);
-				switch (jConfig['type']) {
-					case 'POST':
-						xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;charset=' + jConfig['charset']);
-						break;
-					//case 'HEAD':
-					//xhr.getAllResponseHeaders()
-					//xhr.getResponseHeader("Last-Modified")
-					//	break;
+				if (jConfig['type'] === 'POST') {
+					xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;charset=' + jConfig['charset']);
 				}
 				xhr.onreadystatechange = function () {
 					if (this.readyState === 4) {
 						switch (this.status) {
 							case 200:
 								var xResult;
-								switch (jConfig['dataType']) {
-									case 'JSON':
-										xResult = eval('(' + this.responseText + ')');
+								// GET POST HEAD PUT DELETE CONNECT OPTIONS TRACE
+								switch (jConfig['type']) {
+									case 'HEAD':
+										switch (jConfig['dataType']) {
+											case 'JSON':
+												var aAllResHeader = this.getAllResponseHeaders().split('\r\n');
+												var jAllResHeader = {};
+												for (var i = 0, l = aAllResHeader.length; i < l; i++) {
+													var sResHeader = aAllResHeader[i];
+													var nIndex = sResHeader.indexOf(': ');
+													if (nIndex > -1) {
+														jAllResHeader[sResHeader.substr(0, nIndex)] = sResHeader.substr(nIndex + 2)
+													}
+												}
+												xResult = jAllResHeader;
+												break;
+											case 'FUNCTION':
+												// 自定义的数据类型，只有使用 HEAD 方式时才有
+												var _this = this;
+												xResult = function (sKey) {
+													// _this.getResponseHeader("Last-Modified");
+													return _this.getResponseHeader(sKey);
+												};
+												break;
+											default :
+												xResult = this.getAllResponseHeaders();
+										}
 										break;
-									case 'XML':
-										xResult = this.responseXML;
-										break;
-									default :
-										xResult = this.responseText;
+									default:
+										// JSON XML TEXT
+										switch (jConfig['dataType']) {
+											case 'JSON':
+												xResult = eval('(' + this.responseText + ')');
+												break;
+											case 'XML':
+												xResult = this.responseXML;
+												break;
+											default :
+												xResult = this.responseText;
+										}
 								}
 								jConfig['success'].call(jConfig, xResult, this);
 								break;
